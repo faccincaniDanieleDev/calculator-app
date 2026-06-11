6660d60d0eea981df79be1a083dca8c124d9241a
faccincaniDanieleDev <faccincani.daniele.dev@gmail.com>
2026-06-11 16:35:55 +0200

    feat: add CalculatorApp controller
    Implement CalculatorApp controller connecting CalculatorModel and CalculatorView.

---
+++ src/calculatorApp.py
@@ -0,0 +1,12 @@
+"""Controller: connects CalculatorModel and CalculatorView.
+
+    Args:
+        window: Root tkinter window.
+    """
+    
+import tkinter as tk
+from src.calculatorModel import CalculatorModel
+from src.calculatorView import CalculatorView
+
+class CalculatorApp:
+    pass
